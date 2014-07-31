143c181934388d334c2a2f565f35b308aed1f97e
Maarten van der Velde <maarten.vandervelde@naturalis.nl>
2014-07-31 11:10:19 +0000

    Adding Theme-settings feature

--- all/themes/custom/bioportal_theme/templates/page.tpl.php
+++ all/themes/custom/bioportal_theme/templates/page.tpl.php
@@ -167,17 +167,17 @@
 
       <a id="main-content"></a>
 
-      <?php if ($breadcrumb): print $breadcrumb; endif; ?>
-
       <?php if ($title && !$is_front): ?>
         <?php print render($title_prefix); ?>
-        <h1 id="page-title" class="title"><?php print $title; ?></h1>
+          <h1 id="page-title" class="title"><?php print $title; ?></h1>
         <?php print render($title_suffix); ?>
       <?php endif; ?>
 
       <?php if (!empty($tabs)): ?>
-        <?php print render($tabs); ?>
-        <?php if (!empty($tabs2)): print render($tabs2); endif; ?>
+        <div class="tabs">
+          <?php print render($tabs); ?>
+          <?php if (!empty($tabs2)): print render($tabs2); endif; ?>
+        </div>
       <?php endif; ?>
 
       <?php if ($action_links): ?>
