83b871a0870f98d306cb7457c6814e5a23b9d740
Maarten van der Velde <maarten.vandervelde@naturalis.nl>
2014-08-14 13:03:16 +0000

    adding javascript collapse/expand behaviour to advanced search

--- all/themes/custom/bioportal_theme/js/bioportal_theme.js
+++ all/themes/custom/bioportal_theme/js/bioportal_theme.js
@@ -1,10 +1,39 @@
-(function ($, Drupal) {
+(function ($, Drupal) { Drupal.behaviors.bioportal_theme = { attach: function(context, settings) {
 
-  Drupal.behaviors.bioportal_theme = {
-    attach: function(context, settings) {
-      h = $(window).height();
-      $("main").css("min-height",h-150);
-    }
-  };
+  // Auto fit height of main container
+  h = $(window).height();
+  $("main").css("min-height",h-150);
 
-})(jQuery, Drupal);
+  // SEARCH FORM: hide/show advanced search
+  var $_advanced_search_form = $("#ndabio-advanced-search");
+  var $_omnibox              = $("#edit-ndabio-adv");
+
+  // -- search form: hide advanced search
+  $_advanced_search_form.slideUp(0);
+
+  // -- search form: add dropdown button to omni-search box
+  $_omnibox.wrap("<div class='ndabio-omnibox-wrapper>'</div>");
+
+  // -- search form: behaviour for dropdown button
+  $_omnibox.removeAttr("disabled");
+  $("#edit-submit").removeAttr("disabled");
+  $("<div class='ndabio-toggle-advanced icon-triangle-down' />")
+    .insertAfter( $_omnibox )
+    .click(function(){
+      $("#edit-omnisearch").toggleClass("disabled");
+      $_omnibox.toggleAttr("disabled").toggleClass("disabled");
+      $("#edit-submit").toggleAttr("disabled").toggleClass("disabled");
+      $(this).toggleClass("icon-triangle-down").toggleClass("icon-triangle-up");
+
+    })
+
+
+} }; })(jQuery, Drupal);
+
+jQuery.fn.toggleAttr = function(a, b) {
+    var c = (b === undefined);
+    return this.each(function() {
+        if((c && !jQuery(this).is("["+a+"]")) || (!c && b)) jQuery(this).attr(a,a);
+        else jQuery(this).removeAttr(a);
+    });
+};
