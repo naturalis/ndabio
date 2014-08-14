0a3ce566f02ffca14ee8b1f98f63cce914e4e595
Maarten van der Velde <maarten.vandervelde@naturalis.nl>
2014-08-14 08:06:02 +0000

    Adding inline-class to labels

--- all/themes/custom/bioportal_theme/template.php
+++ all/themes/custom/bioportal_theme/template.php
@@ -79,8 +79,8 @@ function bioportal_theme_form_element($variables) {
   switch ($element['#title_display']) {
     case 'before':
       $output .= "<div class='row'>\n" ;
-      $output .= "\t<div class='small-2 small-offset-2 columns'> " . theme('form_element_label', $variables). "</div>\n";
-      $output .= "\t<div class='small-5 columns end'> " .  $prefix . $element['#children'] . $suffix . "</div>\n";
+      $output .= "\t<div class='large-2 large-offset-2 small-5 columns'> " . theme('form_element_label', $variables). "</div>\n";
+      $output .= "\t<div class='large-5 small-7 columns end'> " .  $prefix . $element['#children'] . $suffix . "</div>\n";
       $output .= "</div>\n";
       break;
     case 'invisible':
@@ -107,4 +107,39 @@ function bioportal_theme_form_element($variables) {
   $output .= $show_wrapper ? "</div>\n" : "";
 
   return $output;
+}
+
+function bioportal_theme_form_element_label($variables) {
+  $element = $variables['element'];
+  // This is also used in the installer, pre-database setup.
+  $t = get_t();
+
+  // If title and required marker are both empty, output no label.
+  if ((!isset($element['#title']) || $element['#title'] === '') && empty($element['#required'])) {
+    return '';
+  }
+
+  // If the element is required, a required marker is appended to the label.
+  $required = !empty($element['#required']) ? theme('form_required_marker', array('element' => $element)) : '';
+
+  $title = filter_xss_admin($element['#title']);
+
+  $attributes = array();
+  // Style the label as class option to display inline with the element.
+  if ($element['#title_display'] == 'after') {
+    $attributes['class'] = 'option';
+  }
+  // Show label only to screen readers to avoid disruption in visual flows.
+  elseif ($element['#title_display'] == 'invisible') {
+    $attributes['class'] = 'element-invisible';
+  } elseif ( $element["#title_display"] == "before" ){
+    $attributes['class'] = 'inline';
+  }
+
+  if (!empty($element['#id'])) {
+    $attributes['for'] = $element['#id'];
+  }
+
+ // The leading whitespace helps visually separate fields from inline labels.
+  return ' <label' . drupal_attributes($attributes) . '>' . $t('!title !required', array('!title' => $title, '!required' => $required)) . "</label>\n";
 }
