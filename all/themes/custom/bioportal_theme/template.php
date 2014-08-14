bb8d6c135f8d8f9986b3e31674df1d653f35bbf4
Maarten van der Velde <maarten.vandervelde@naturalis.nl>
2014-08-14 07:43:23 +0000

    adding grid classes to form-elements (moving them from Naturalis theme to Bioportal theme)

--- all/themes/custom/bioportal_theme/template.php
+++ all/themes/custom/bioportal_theme/template.php
@@ -31,36 +31,80 @@ function bioportal_theme_ndabio_omnisearch(&$variables){
   $output .= "  </div>";
   $output .= "</div>";
 
-  // $output ='<div class="row collapse">
-  //   <div class="small-3 large-2 columns">
-  //     <span class="prefix">http://</span>
-  //   </div>
-  //   <div class="small-9 large-10 columns">
-  //     <input type="text" placeholder="Enter your URL...">
-  //   </div>
-  // </div>
-  // <div class="row">
-  //   <div class="large-6 columns">
-  //     <div class="row collapse">
-  //       <div class="small-10 columns">
-  //         <input type="text" placeholder="Hex Value">
-  //       </div>
-  //       <div class="small-2 columns">
-  //         <a href="#" class="button postfix">Go</a>
-  //       </div>
-  //     </div>
-  //   </div>
-  //   <div class="large-6 columns">
-  //     <div class="row collapse">
-  //       <div class="small-9 columns">
-  //         <input type="text" placeholder="Value">
-  //       </div>
-  //       <div class="small-3 columns">
-  //         <span class="postfix radius">Label</span>
-  //       </div>
-  //     </div>
-  //   </div>
-  // </div>';
+  return $output;
+}
+
+function bioportal_theme_form_element($variables) {
+  $element = &$variables['element'];
+  $show_wrapper = TRUE;
+
+  if ( isset($element['#nowrapper'])){
+    if ( $element['#nowrapper'] == "true"){
+      $show_wrapper = FALSE;
+    }
+  };
+
+  // This function is invoked as theme wrapper, but the rendered form element
+  // may not necessarily have been processed by form_builder().
+  $element += array(
+    '#title_display' => 'before',
+  );
+
+  // Add element #id for #type 'item'.
+  if (isset($element['#markup']) && !empty($element['#id'])) {
+    $attributes['id'] = $element['#id'];
+  }
+  // Add element's #type and #name as class to aid with JS/CSS selectors.
+  $attributes['class'] = array('form-item');
+  if (!empty($element['#type'])) {
+    $attributes['class'][] = 'form-type-' . strtr($element['#type'], '_', '-');
+  }
+  if (!empty($element['#name'])) {
+    $attributes['class'][] = 'form-item-' . strtr($element['#name'], array(' ' => '-', '_' => '-', '[' => '-', ']' => ''));
+  }
+
+  // Add a class for disabled elements to facilitate cross-browser styling.
+  if (!empty($element['#attributes']['disabled'])) {
+    $attributes['class'][] = 'form-disabled';
+  }
+  $output = $show_wrapper ?  '<div' . drupal_attributes($attributes) . '>' . "\n" : '';
+
+  // If #title is not set, we don't display any label or required marker.
+  if (!isset($element['#title'])) {
+    $element['#title_display'] = 'none';
+  }
+  $prefix = isset($element['#field_prefix']) ? '<span class="field-prefix">' . $element['#field_prefix'] . '</span> ' : '';
+  $suffix = isset($element['#field_suffix']) ? ' <span class="field-suffix">' . $element['#field_suffix'] . '</span>' : '';
+
+  switch ($element['#title_display']) {
+    case 'before':
+      $output .= "<div class='row'>\n" ;
+      $output .= "\t<div class='small-2 small-offset-2 columns'> " . theme('form_element_label', $variables). "</div>\n";
+      $output .= "\t<div class='small-5 columns end'> " .  $prefix . $element['#children'] . $suffix . "</div>\n";
+      $output .= "</div>\n";
+      break;
+    case 'invisible':
+      $output .= ' ' . theme('form_element_label', $variables);
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+
+    case 'after':
+      $output .= ' ' . $prefix . $element['#children'] . $suffix;
+      $output .= ' ' . theme('form_element_label', $variables) . "\n";
+      break;
+
+    case 'none':
+    case 'attribute':
+      // Output no label and no required marker, only the children.
+      $output .= ' ' . $prefix . $element['#children'] . $suffix . "\n";
+      break;
+  }
+
+  if (!empty($element['#description'])) {
+    $output .= '<div class="description">' . $element['#description'] . "</div>\n";
+  }
+
+  $output .= $show_wrapper ? "</div>\n" : "";
 
   return $output;
 }
