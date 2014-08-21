1a5b3cb9a499b5b53fa7e50439956911e779aba8
Maarten van der Velde <maarten.vandervelde@naturalis.nl>
2014-08-21 06:37:17 +0000

    Added 'geographic search' banner & link to front page

--- all/themes/custom/bioportal_theme/templates/page.tpl.php
+++ all/themes/custom/bioportal_theme/templates/page.tpl.php
@@ -165,6 +165,16 @@
       <?php endif; ?>
 
       <?php print render($page['content']); ?>
+
+      <?php if ($is_front): ?>
+        <div id="banner-geographical-search">
+          <a href="geographic-search">
+            <h3><?php print t("Geographic Search"); ?></h3>
+            <img src='sites/all/themes/custom/bioportal_theme/img/geographic_search.png' />
+          </a>
+        </div>
+      <?php endif; ?>
+
     </div>
     <!--/.main region -->
 
