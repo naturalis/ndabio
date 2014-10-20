c637975581412f5913458ad2c761897d7233bdc2
Maarten van der Velde <maarten.vandervelde@naturalis.nl>
2014-10-20 08:15:13 +0000

    Adding templates and template-suggestions for 'headless' viewing of views

--- all/themes/custom/bioportal_theme/template.php
+++ all/themes/custom/bioportal_theme/template.php
@@ -19,6 +19,9 @@
 function bioportal_theme_preprocess_page(&$variables) {
   $block = module_invoke('boxes', 'block_view', 'site_explanation');
   $variables['intro'] = $block['content'];
+
+  _embeded_view($variables);
+
 }
 
 function bioportal_theme_ndabio_omnisearch(&$variables){
@@ -26,12 +29,32 @@ function bioportal_theme_ndabio_omnisearch(&$variables){
 
   $output  = "<div class='row collapse'>";
   $output .= "  <div class='small-9 large-6 large-offset-2 columns'>";
-  $output .= drupal_render( $element['term']);
+  $output .=      drupal_render( $element['term']);
   $output .= "  </div>";
   $output .= "  <div class='small-3 large-2 end columns'>";
-  $output .= drupal_render( $element['submit']);
+  $output .=      drupal_render( $element['submit']);
   $output .= "  </div>";
   $output .= "</div>";
 
   return $output;
+}
+
+/**
+ * Implements template_preprocess_page
+ *
+ * completely remove all the HTML output around the page content in Drupal 7 by
+ * implementing both hook_preprocess_page and hook_preprocess_html
+ *
+ */
+
+function _embeded_view(&$variables) {
+  if (isset($_GET['response_type']) && $_GET['response_type'] == 'embed') {
+    $variables['theme_hook_suggestions'][] = 'page__embed';
+  }
+}
+
+function bioportal_theme_preprocess_html(&$variables) {
+  if (isset($_GET['response_type']) && $_GET['response_type'] == 'embed') {
+    $variables['theme_hook_suggestions'][] = 'html__embed';
+  }
 }
