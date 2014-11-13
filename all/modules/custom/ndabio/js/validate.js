f0943feec6d3b8529fac843a03b1d23809b88a12
Maarten van der Velde <maarten.vandervelde@naturalis.nl>
2014-11-13 15:33:15 +0000

    JS: moving validation function to sites/all/modules/custom/ndabio/js/validate.js

---
+++ all/modules/custom/ndabio/js/validate.js
@@ -0,0 +1,53 @@
+(function ($, Drupal) { Drupal.behaviors.validate = { attach: function(context, settings) {
+
+  // ---------------------------------------------------------
+  // SEARCH FORM: form validation
+  // ---------------------------------------------------------
+
+  $("#ndabio-advanced-taxonomysearch").submit(function(){
+    
+    var $_omnibox              = $("#edit-term");
+
+    int_valid = 0;
+
+    // If there's more then one textfield with containing more
+    // then three characters:
+    $(this).find('input[type=text], select').each(function(){
+      str_val = $(this).val();
+      if( str_val != "" && str_val.length > 2) {
+        int_valid += 1;
+      }
+    });
+
+    // 'select boxes' also count as valid choices, ofcourse
+    $(this).find('select').each(function(){
+      str_val = $(this).val();
+      if( str_val != "" ) {
+        int_valid += 1;
+      }
+    });
+
+    // If the simple search is entered, but too short:
+    str_val = $($_omnibox).val();
+    if( str_val != "" && str_val.length < 2){
+      int_valid = 0;
+    } 
+
+    if (int_valid < 1){
+      
+      alert(
+        "Please, make sure that you complete \n" +
+        "at least one field and that it contains more\n" +
+        "then three characters.\n" 
+      );
+
+      return false;
+
+    } else {
+      
+      //preloader();
+
+    }
+
+  });
+} }; })(jQuery, Drupal);
