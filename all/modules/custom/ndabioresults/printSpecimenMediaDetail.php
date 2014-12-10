bb4650a48b5e7e287a68e79a27907ad59292c91a
ruud-altenburg <ruud.altenburg@naturalis.nl>
2014-12-10 15:43:37 +0100

    mp4 support (niet in FF Mac...)

--- all/modules/custom/ndabioresults/printSpecimenMediaDetail.php
+++ all/modules/custom/ndabioresults/printSpecimenMediaDetail.php
@@ -4,14 +4,22 @@ function printSpecimenMediaDetail ($data) {
 //p($data);
 	$output = "<h2>" . $data['title'] . "</h2>\n";
 	$output .= printNavigation($data);
-	$output .= "<img src='" . $data['imgSrc'] . "' alt='" . $data['title'] .
-		"' title=''" . $data['title'] . " />\n";
+echo $data['imgSrc'];
+
+    if (isMp4($data['imgSrc'])) {
+        $output .= '<video src="' . $data['imgSrc'] . '" type="video/mp4" autoplay controls></video>';
+    } else {
+	   $output .= "<img src='" . $data['imgSrc'] . "' alt='" . $data['title'] .
+	       "' title=''" . $data['title'] . " />\n";
+    }
+
 	$output .= "<div class='property-list'>";
 
 	if (!empty($data['unitID'])) {
 		$output .= printDL(
             ucfirst(translateNdaField('unitID')),
-            '<a href="' . printDrupalLink(specimenDetailService() . '?unitID=' . $data['unitID']) . '">' . $data['unitID'] . '</a>'
+            '<a href="' . printDrupalLink(specimenDetailService() . '?unitID=' .
+                $data['unitID']) . '">' . $data['unitID'] . '</a>'
 		);
 	}
 	if (!empty($data['names'])) {
