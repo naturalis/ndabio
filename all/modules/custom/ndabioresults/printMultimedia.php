8f401210b0b2763fb5acebe7afab1fff6e5997e0
ruud-altenburg <ruud.altenburg@naturalis.nl>
2014-12-16 16:12:38 +0100

    betere image caption

--- all/modules/custom/ndabioresults/printMultimedia.php
+++ all/modules/custom/ndabioresults/printMultimedia.php
@@ -29,20 +29,23 @@ function printMultimedia ($data) {
 
         // Reset imgUrl if media is mp4
         if ($row['format'] == 'video/mp4') {
-            $row['imgSrc'] = setBasePath() . 'profiles/naturalis/themes/custom/naturalis_theme/images/naturalis/play.png';
+            $row['imgSrc'] = setBasePath() .
+                'profiles/naturalis/themes/custom/naturalis_theme/images/naturalis/play.png';
         }
+
+        $mmHit = printMultimediaHit($row);
 
         $output .=
           "<a class='polaroid' href='" . printDrupalLink($row['url']). "' title='" . $row['title'] . "'>" .
           "  <div class='polaroid-image' style='background-image: url(" . $row['imgSrc']. ");' alt='" . $row['title'] . "'></div>" .
           "  <div class='polaroid-caption'>".
           "    <div class='image-title'>" . (!empty($row['taxon']) ? $row['taxon'] : '&mdash;') .
-                    (!empty($row['unitID']) ? '<br>' . $row['unitID'] : '') . "</div>".
-          // "    <div class='image-title'>" . $row['caption'] . "</div>".
-           "    <div class='image-hits'>" . $row['caption'] .  "</div>".
-           "    <div class='image-hits'>" . printMultimediaHit($row) .  "</div>".
-           // "    <div class='image-source'>" . $row['source'] . "</div>".
-          "  </div>".
+                    (!empty($row['unitID']) ? '<br>' . $row['unitID'] : '') . "</div>";
+         $output .= (strpos($row['unitID'], $row['caption']) === false) ?
+           "    <div class='image-hits'>" . $row['caption'] . "</div>" : '';
+         $output .= $mmHit ?
+           "    <div class='image-hits'>" . $mmHit ."</div>" : '';
+         $output .= "  </div>" .
           "</a>";
     }
 
