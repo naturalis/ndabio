65d7de58aecf8d6e6cbe63031da380a43ae92f35
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2015-04-10 15:14:41 +0200

    prettyPhoto

--- all/modules/custom/ndabioresults/printTaxonMediaDetail.php
+++ all/modules/custom/ndabioresults/printTaxonMediaDetail.php
@@ -6,14 +6,23 @@
  * @return string Formatted output
  */
 function printTaxonMediaDetail ($data) {
-
 //p($data);
 
     $output  = _wrap( t("Media item")   , "div", "category");
     $output .= _wrap( '', "h2"  );
 
-    $output .="<img src='" . $data['imgSrc'] . "' alt='" . $data['title'] .
-    	"' title=''" . $data['title'] . ">";
+    $altParts = array(
+        isset($data['names'][0]['name']) ? strip_tags($data['names'][0]['name']) : '',
+        isset($data['caption']) ? strip_tags($data['caption']) : ''
+    );
+    $alt = implode(' | ', array_filter($altParts));
+
+    $img = "<img src='" . $data['imgSrc'] . "' alt='$alt' title='$alt' />";
+    if (loadPrettyPhoto($data['imgSrc'])) {
+        $img = "<a href='" . $data['imgSrc'] . "' rel='prettyPhoto'>$img</a>\n";
+    }
+
+    $output .= $img;
 
     $output .= "<div class='property-list'>";
     $output .= printNamesWithLinks($data['names'], t('Scientific name'));
