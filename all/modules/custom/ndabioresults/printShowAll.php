f2d9f376f14cf5d38ff88e80214104144a9618c2
root <root@nba-drupal-004.cloud.naturalis.nl>
2015-07-01 14:06:37 +0000

    updated some files, live version bioportal release3

--- all/modules/custom/ndabioresults/printShowAll.php
+++ all/modules/custom/ndabioresults/printShowAll.php
@@ -6,11 +6,12 @@
  * @return string Formatted output
  */
 function printShowAll ($data) {
+    global $language;
     $output = '';
     if (!isset($data['showAll']) || empty($data['showAll'])) {
         return $output;
     }
-    $output = '<div class="show-all"><a href="?nba_request=' . $data['showAll'] . '">' .
+    $output = '<div class="show-all"><a href="?nba_request=' . $data['showAll'] . '&language=' . $language->language . '">' .
         '<i class="icon-arrow-right"></i>'.
         t('Show all') . ' ' . _formatNumber($data['total']) . ' '. t('results') . '</a></div>';
     return $output;
