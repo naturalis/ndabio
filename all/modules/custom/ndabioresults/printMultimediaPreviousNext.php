adfb8a5909bd18d9e869ed3cdb690f172dfba2ef
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2015-07-02 13:17:32 +0200

    Multimedia navigatie werkt!

--- all/modules/custom/ndabioresults/printMultimediaPreviousNext.php
+++ all/modules/custom/ndabioresults/printMultimediaPreviousNext.php
@@ -1,5 +1,7 @@
 <?php
 function printMultimediaPreviousNext () {
+//p($_SESSION['ndaNavigation']['media']);
+
     // Return "link-less" navigation if request is empty or history is not yet set
     if (!isset($_GET['nba_request']) ||
         !isset($_SESSION['ndaNavigation']['media']['currentSet'])) {
@@ -9,16 +11,43 @@ function printMultimediaPreviousNext () {
     }
 
     $set = $_SESSION['ndaNavigation']['media']['currentSet'];
+    $offset = $_SESSION['ndaNavigation']['media']['offset'];
     $key = array_search(urldecode($_GET['nba_request']),
         $_SESSION['ndaNavigation']['media']['currentSet']);
-    $previousUrl = isset($set[$key - 1]) ? urldecode($set[$key - 1]) : false;
-    $nextUrl = isset($set[$key + 1]) ? urldecode($set[$key + 1]) : false;
+
+    //$previousUrl = isset($set[$key - 1]) ? urldecode($set[$key - 1]) : false;
+    //$nextUrl = isset($set[$key + 1]) ? urldecode($set[$key + 1]) : false;
+
+    $previousUrl = false;
+    // Scroll through set
+    if (isset($set[$key - 1])) {
+        $previousUrl = urldecode($set[$key - 1]);
+    // First item of set reached; check if there's a previousSet.
+    // If so, reset $_SESSION['ndaNavigation']['media']
+    } else if (!empty($_SESSION['ndaNavigation']['media']['previousSet'])) {
+        updatePreviousNext($_SESSION['ndaNavigation']['media']['previousSet']);
+        $previousUrl = end($_SESSION['ndaNavigation']['media']['currentSet']);
+        //$key = array_search($previousUrl, $_SESSION['ndaNavigation']['media']['currentSet']);
+    }
+
+    $nextUrl = false;
+    // Scroll through set
+    if (isset($set[$key + 1])) {
+        $nextUrl = urldecode($set[$key + 1]);
+    // Last item of set reached; check if there's a nextSet.
+    } else if (!empty($_SESSION['ndaNavigation']['media']['nextSet'])) {
+//p($_SESSION['ndaNavigation']['media']);
+        updatePreviousNext($_SESSION['ndaNavigation']['media']['nextSet']);
+//p($_SESSION['ndaNavigation']['media']);
+        $nextUrl = reset($_SESSION['ndaNavigation']['media']['currentSet']);
+        //$key = array_search($nextUrl, $_SESSION['ndaNavigation']['media']['currentSet']);
+    }
 
     $output = '<p class="button-bar">';
 
     $output .= '<span class="button-text">' . sprintf(
         t('%d of %d'),
-        $_SESSION['ndaNavigation']['media']['offset'] + $key + 1,
+        $offset + $key + 1,
         $_SESSION['ndaNavigation']['media']['total']
     ) . '</span>';
 
