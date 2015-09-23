566e3e41bca36a504322d8a947fa1dc9f3f97bb9
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2015-09-23 14:41:38 +0200

    BIOP-524

--- all/modules/custom/ndabio/js/map.js
+++ all/modules/custom/ndabio/js/map.js
@@ -77,6 +77,7 @@ function initialize() {
 	var mapOptions = {
 		center: new google.maps.LatLng(setMapCenterLat(), setMapCenterLon()),
 		mapTypeId: 'satellite',
+		streetViewControl: false,
 		zoom: setZoomLevel()
 	};
 	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
@@ -182,6 +183,7 @@ function initializeSpecimens() {
 	var mapOptions = {
 		  center: new google.maps.LatLng(setMapCenterLat(), setMapCenterLon()),
 		  mapTypeId: 'satellite',
+		  streetViewControl: false,
 		  zoom: setZoomLevel()
 		};
 	map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
