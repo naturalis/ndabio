9ea2d0a5e21fc1647ee1cf4984d1695b251008f4
maarten schermer <maarten.schermer@naturalis.nl>
2017-02-23 16:25:44 +0100

    ?

--- all/modules/custom/nba_dashboard/nba_dashboard.js
+++ all/modules/custom/nba_dashboard/nba_dashboard.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function(){
 
    // jQuery methods go here...
-   jQuery('#dashboard').text("Hello world!");
+   jQuery('#dashboard').text("Hello world!?");
 
 });
 <div style="border:1px solid #666;width:100%;height:100%;">test</div>
