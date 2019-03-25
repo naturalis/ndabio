cf4d0ec629160b0def4ed2a35e8e107637c3f188
Ruud Altenburg <ruud.altenburg@naturalis.nl>
2019-03-25 17:25:57 +0100

    Fix config

--- all/modules/custom/ndabioresults/js/ajax.js
+++ all/modules/custom/ndabioresults/js/ajax.js
@@ -1,29 +1,35 @@
-function getBhlReferences (term) {
-	// Separate NBA request from what is passed on to callback function
-	jQuery.ajax({
-		url: str_base_path + 'ajax',
-		type: "GET",
-		dataType: "json",
-		data: ({
-			term: term,
-			source: 'bhl'
-		}),
-		success: function (data) {
-			printBhlReferences(data, term);
-		}
+function getReferences (term) {
+	var sources = {
+		'wc': 'WorldCat',
+		'bhl': 'Biodiversity Heritage Library'
+	};
+	jQuery.each(sources, function(source, name) {
+		jQuery.ajax({
+			url: str_base_path + 'ajax',
+			type: "GET",
+			dataType: "json",
+			data: ({
+				term: term,
+				source: source
+			}),
+			success: function (data) {
+				printReference(data, term, source, name);
+			}
+		});
 	});
 }
 
-function printBhlReferences (data, term) {
+function printReference (data, term, source, name) {
 	var output = '';
 	for (let i = 0; i < data.references.length; i++) {
-		output += '<p><a href="' + data.references[i].url + '" target="_blank">' + data.references[i].title + '</a></p>';
+		output += '<p style="margin: 10px 0;"><a href="' + data.references[i].url + '" target="_blank">' + data.references[i].title + '</a></p>';
 	};
 	if (output == '') {
-		output += '<p>' + Drupal.t('No results for BHL catalogue search') + '</p>';
+		output += '<p>' + Drupal.t('No results for ' + name) + '</p>';
+	} else {
+		output = '<p><strong>References found in ' + name + '</strong>:</p>' + output +
+			'<div style="margin: 12px 0 30px 0;"><a href="' + data.source_url + '" target="_blank">' +
+			'<i class="icon-arrow-right"></i>' + Drupal.t('Show all results for') + ' ' + name + '</a></div>';
 	}
-	output += '<div style="margin-top: 12px;"><a href="https://www.biodiversitylibrary.org/search?searchTerm=' +
-		term + '&stype=F" target="_blank">' + '<i class="icon-arrow-right"></i>' +
-		Drupal.t('BHL full-text search') + '</a></div>';
-	jQuery('#literature').html(output);
+	jQuery('#' + source).html(output);
 }
